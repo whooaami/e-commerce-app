7f7e5aa2a1c48e83854a0c52a58b2f90cc12e6f9
whooaami <yarkos03@gmail.com>
2024-04-17 14:05:24 +0300

    Create app for Order

---
+++ api-module/apps/order/managers.py
@@ -0,0 +1,5 @@
+from apps.abstract.managers import AbstractManager
+
+
+class OrderManager(AbstractManager):
+    pass
---
+++ api-module/apps/order/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from apps.abstract.models import AbstractModel
+from apps.order.managers import OrderManager
+
+
+class Order(AbstractModel):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Processing', 'Processing'),
+        ('Shipped', 'Shipped'),
+        ('Delivered', 'Delivered'),
+        ('Cancelled', 'Cancelled'),
+    ]
+
+    products = models.ForeignKey(to='product.Product', on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+    shipping_address = models.TextField()
+
+    objects = OrderManager()
+
+    def __str__(self):
+        return f'Order #{self.id}'
