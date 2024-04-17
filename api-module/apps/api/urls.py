30db0314372435bf846f004c27e26a9a5b4fc97e
whooaami <yarkos03@gmail.com>
2024-04-17 14:04:15 +0300

    Create files for API

---
+++ api-module/apps/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import (
+    path,
+    include
+)
+
+
+urlpatterns = [
+    # /api/...
+    path('auth/', include('apps.authentication.api.urls')),
+    path('users/', include('apps.user.api.urls')),
+    path('orders/', include('apps.order.api.urls')),
+    path('products/', include('apps.product.api.urls')),
+    path('saved-list/', include('apps.wishlist.api.urls')),
+]
