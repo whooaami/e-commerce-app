cb684e002835a8d0e39d30f84a1fc9052bc48639
whooaami <yarkos03@gmail.com>
2024-04-22 14:58:37 +0300

    Add actions for cart. Fix style

--- ui-module/src/pages/Cart.jsx
+++ ui-module/src/pages/Cart.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Row, Col, Card, Button } from "react-bootstrap";
-import useSWR from "swr";
+import { Table, Button, Form } from "react-bootstrap";
+import useSWR, { mutate } from "swr";
 import { fetcher } from "../helpers/axios";
+import { getUser, getAccessToken } from "../hooks/user.actions";
 import Layout from "../components/Layout";
 import BackButton from "../components/BackButton";
 
@@ -14,6 +15,47 @@ function Cart() {
       setLoading(false);
     }
   }, [cartData]);
+
+  const handleDeleteItem = async (id) => {
+    try {
+      const url = `http://localhost:8000/api/cart/${id}/`;
+      console.log("Sending request to:", url);
+      const response = await fetch(url, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${getAccessToken()}`,
+        },
+        body: JSON.stringify({
+          user: getUser().id,
+          product: id,
+        }),
+      });
+      mutate("/cart/");
+    } catch (error) {
+      console.error("Error deleting item:", error);
+    }
+  };
+
+  const handleQuantityChange = async (id, quantity) => {
+    try {
+      const url = `http://localhost:8000/api/cart/${id}/`;
+      console.log("Sending request to:", url);
+      const response = await fetch(url, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${getAccessToken()}`,
+        },
+        body: JSON.stringify({
+          quantity: quantity,
+        }),
+      });
+      mutate("/cart/");
+    } catch (error) {
+      console.error("Error updating quantity:", error);
+    }
+  };
 
   if (cartError) {
     return <div>Error fetching cart!</div>;
@@ -30,20 +72,51 @@ function Cart() {
       </div>
       <h2 className="text-center mb-4">Shopping Cart</h2>
       {cartData && cartData.length > 0 ? (
-        <Row className="justify-content-center">
-          {cartData.map((cartItem) => (
-            <Col key={cartItem.id} xs={12} sm={6} md={4} lg={3} className="mb-4">
-              <Card>
-                <Card.Img variant="top" src={cartItem.product_info.image} />
-                <Card.Body>
-                  <Card.Title>{cartItem.product_info.name}</Card.Title>
-                  <Card.Text>Price: {cartItem.product_info.price}</Card.Text>
-                  <Card.Text>Quantity: {cartItem.quantity}</Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Quantity</th>
+              <th>Price</th>
+              <th>Product</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartData.map((cartItem) => (
+              <tr key={cartItem.id}>
+                <td>{cartItem.product_info.name}</td>
+                <td>
+                  <Form.Control
+                    type="number"
+                    value={cartItem.quantity}
+                    onChange={(e) =>
+                      handleQuantityChange(cartItem.id, e.target.value)
+                    }
+                    style={{ width: "60px" }}
+                    placeholder={cartItem.product_info.quantity}
+                  />
+                </td>
+                <td>{cartItem.product_info.price}</td>
+                <td>
+                  <img
+                    src={cartItem.product_info.image}
+                    alt={cartItem.product_info.name}
+                    style={{ width: "50px", height: "50px" }}
+                  />
+                </td>
+                <td>
+                  <Button
+                    variant="danger"
+                    onClick={() => handleDeleteItem(cartItem.id)}
+                  >
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       ) : (
         <div className="text-center mt-5">
           <h3>No products in the cart</h3>
