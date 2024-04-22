9a1f38beb7c3282883165aa27368df88bc56b983
whooaami <yarkos03@gmail.com>
2024-04-22 17:04:14 +0300

    Fix by linter

--- api-module/apps/product/admin.py
+++ api-module/apps/product/admin.py
@@ -7,25 +7,34 @@ from apps.product.models import Category, Product
 class ImagePreviewMixin:
     def image_preview(self, obj, width=50, height=50):
         if obj.image:
-            return mark_safe('<img src="{url}" width="{width}" height="{height}" />'.format(
-                url=obj.image.url,
-                width=width,
-                height=height,
-            ))
+            return mark_safe(
+                '<img src="{url}" width="{width}" height="{height}" />'.format(
+                    url=obj.image.url,
+                    width=width,
+                    height=height,
+                )
+            )
         else:
-            return '(No image)'
+            return "(No image)"
 
-    image_preview.short_description = 'Image Preview'
+    image_preview.short_description = "Image Preview"
 
 
 class CategoryAdmin(admin.ModelAdmin, ImagePreviewMixin):
-    list_display = ['name', 'image_preview']
-    readonly_fields = ('image_preview',)
+    list_display = ["name", "image_preview"]
+    readonly_fields = ("image_preview",)
 
 
 class ProductAdmin(admin.ModelAdmin, ImagePreviewMixin):
-    list_display = ['name', 'description', 'price', 'quantity', 'category', 'image_preview']
-    readonly_fields = ('image_preview',)
+    list_display = [
+        "name",
+        "description",
+        "price",
+        "quantity",
+        "category",
+        "image_preview",
+    ]
+    readonly_fields = ("image_preview",)
 
 
 admin.site.register(Category, CategoryAdmin)
